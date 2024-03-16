251e36fd458c8c4bca61bbbbbefb39ec1616ab80
Rasyid <und3rd0g.rev3nge@gmail.com>
2024-03-16 12:42:51 +0700

    Add fallback value if pattern not found

--- data_extractor.py
+++ data_extractor.py
@@ -1,6 +1,5 @@
 import locale
 import re
-import logging
 
 
 from datetime import datetime
@@ -34,55 +33,67 @@ class PdfExtractor:
                                 pattern=p_ranking, 
                                 string=pdf_content).group()).group().strip()
         match self._docs_format():
-            case 'format_lama':
-                try:                    
-                    p_valid_until = r"tanggal(\s([0-9]+ [A-Za-z]+\s)+)[0-9]+\s"
-                    p_validated_at = r"Jakarta(\n([0-9]+ [A-Za-z]+\s)+)[0-9]+"
-                    s_valid_until = re.search(pattern=p_valid_until, string=pdf_content).group()
-                    s_validated_at = re.search(pattern=p_validated_at, string=pdf_content).group()
-                    valid_until = re.search(pattern=date_pattern, string=s_valid_until).group()
-                    validated_at = re.search(pattern=date_pattern, string=s_validated_at).group()
-                    locale.setlocale(locale.LC_ALL, 'id_ID')
-                    d_valid_until = datetime.strptime(valid_until, '%d %B %Y').date()
-                    d_validated_at = datetime.strptime(validated_at, '%d %B %Y').date()                
-                    extracted_data = {
-                        'npsn': npsn,
-                        'no_cert': no_cert,
-                        'ranking': ranking,
-                        'validated_at': d_validated_at,
-                        'valid_until': d_valid_until,
-                        'cert_model': self._docs_format(),
-                        'filepath': self.pdf_path
-                        }
-                    
-                    return extracted_data
-                except AttributeError:
-                    logging.error(f"{self.pdf_path} text parsing error")
-            
-            case 'format_baru':
+            case 'format_lama':                        
+                p_valid_until = r"tanggal(\s([0-9]+ [A-Za-z]+\s)+)[0-9]+\s"
+                p_validated_at = r"Jakarta(\n([0-9]+ [A-Za-z]+\s)+)[0-9]+"
+                locale.setlocale(locale.LC_ALL, 'id_ID')
+                
                 try:
-                    p_valid_until = r"dengan tanggal(\s([0-9]+ [A-Za-z]+\s)+)[0-9]+\s"
-                    p_validated_at = r"Pada tanggal [0-9]+ [A-Za-z]+ [0-9]+"
                     s_valid_until = re.search(pattern=p_valid_until, string=pdf_content).group()
-                    s_validated_at = re.search(pattern=p_validated_at, string=pdf_content).group()
                     valid_until = re.search(pattern=date_pattern, string=s_valid_until).group()
-                    validated_at = re.search(pattern=date_pattern, string=s_validated_at).group()                    
-                    locale.setlocale(locale.LC_ALL, 'id_ID')
-                    d_valid_until = datetime.strptime(valid_until, '%d %B %Y').date()
-                    d_validated_at = datetime.strptime(validated_at, '%d %B %Y').date()                
-                    extracted_data = {
-                        'npsn': npsn,
-                        'no_cert': no_cert,
-                        'ranking': ranking,
-                        'validated_at': d_validated_at,
-                        'valid_until': d_valid_until,
-                        'cert_model': self._docs_format(),
-                        'filepath': self.pdf_path
-                        }
-                    
-                    return extracted_data
+                    d_valid_until = datetime.strptime(valid_until, '%d %B %Y').date()                
                 except AttributeError:
-                    logging.error(f"{self.pdf_path} text parsing error")
+                    d_valid_until = 'parse_error'
+                
+                try:
+                    s_validated_at = re.search(pattern=p_validated_at, string=pdf_content).group()
+                    validated_at = re.search(pattern=date_pattern, string=s_validated_at).group()
+                    d_validated_at = datetime.strptime(validated_at, '%d %B %Y').date()                
+                except AttributeError:
+                    d_validated_at = 'parse_error'
+                
+                extracted_data = {
+                    'npsn': npsn,
+                    'no_cert': no_cert,
+                    'ranking': ranking,
+                    'validated_at': d_validated_at,
+                    'valid_until': d_valid_until,
+                    'cert_model': self._docs_format(),
+                    'filepath': self.pdf_path
+                    }
+                
+                return extracted_data                
+            
+            case 'format_baru':                
+                p_valid_until = r"dengan tanggal(\s([0-9]+ [A-Za-z]+\s)+)[0-9]+\s"
+                p_validated_at = r"Pada tanggal [0-9]+ [A-Za-z]+ [0-9]+"
+                locale.setlocale(locale.LC_ALL, 'id_ID')
+                
+                try:
+                    s_valid_until = re.search(pattern=p_valid_until, string=pdf_content).group()
+                    valid_until = re.search(pattern=date_pattern, string=s_valid_until).group()
+                    d_valid_until = datetime.strptime(valid_until, '%d %B %Y').date()
+                except AttributeError:
+                    d_valid_until = 'parse_error'
+                
+                try:
+                    s_validated_at = re.search(pattern=p_validated_at, string=pdf_content).group()
+                    validated_at = re.search(pattern=date_pattern, string=s_validated_at).group()                    
+                    d_validated_at = datetime.strptime(validated_at, '%d %B %Y').date()                
+                except AttributeError:
+                    d_validated_at = 'parse_error'
+                
+                extracted_data = {
+                    'npsn': npsn,
+                    'no_cert': no_cert,
+                    'ranking': ranking,
+                    'validated_at': d_validated_at,
+                    'valid_until': d_valid_until,
+                    'cert_model': self._docs_format(),
+                    'filepath': self.pdf_path
+                    }
+                
+                return extracted_data
         
 
 
